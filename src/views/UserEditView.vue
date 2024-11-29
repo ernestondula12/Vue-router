b97f04ab19f95ac09005470a52cdecd2c025be43
Ernesto Ndula <meteusrosa704@gmail.com>
2024-11-29 11:53:59 +0100

    Primeiro commit

---
+++ src/views/UserEditView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{ $route.query.page }}
+        Úsuario <br>
+
+
+        <button @click="login()">
+            login
+        </button>
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    created(){
+        console.log(this.$route);
+    },
+    methods:{
+        login(){
+            this.$router.push('/about');
+            console.log('Chegou...');
+        }
+    }
+}
+</script>
